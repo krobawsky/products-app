a5957f348b3de98935065a11de5a1965215a4c0e
Ricardo Berrospi <krobawsky.dev@gmail.com>
2024-05-13 12:27:16 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  product_api: "https://b151-190-237-37-34.ngrok-free.app/", // localhost:3000
+  product_api: "http://localhost:3000/", // https://b151-190-237-37-34.ngrok-free.app/
   ngrok_skip_warning: "anything"
 };
